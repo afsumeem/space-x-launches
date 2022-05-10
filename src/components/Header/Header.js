bd1b69c14536eeeadf19f8d3c9d955014ac23322
afsumeem <afsumeem@gmail.com>
2022-05-10 22:31:37 +0600

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,24 +13,23 @@ const Header = ({ allLaunches, setDisplayedRocket, setInputValue, inputValue }) 
 
     return (
         <div>
-            <Navbar bg="dark" expand="lg">
+            <Navbar bg="dark" expand="lg"  >
                 <Container fluid>
-                    <Navbar.Brand href="#" className='text-white'>SpaceX</Navbar.Brand>
+                    <Navbar.Brand href="#" className='text-white fs-2 fw-bold'>SpaceX</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
                             className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Nav.Link href="#action1">Home</Nav.Link>
+                            {/* <Nav.Link href="#home" className='text-white ms-5'>Home</Nav.Link> */}
 
                         </Nav>
                         <Form className="d-flex" onSubmit={handleRocketSearch}>
                             <FormControl
                                 onChange={(e) => setInputValue(e.target.value)}
                                 type="search"
-                                placeholder="Search"
+                                placeholder="Search Rocket"
                                 className="me-2"
                                 aria-label="Search"
                             />
